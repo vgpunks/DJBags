43604a5aca350b2c554ad8002099e89d27ee49ec
vgpunks <4714318+vgpunks@users.noreply.github.com>
2025-07-15 00:35:26 -0400

    Add Warband bank tab

---
+++ src/bank/Warband.lua
@@ -0,0 +1,31 @@
+local ADDON_NAME, ADDON = ...
+
+local bank = {}
+bank.__index = bank
+
+function DJBagsRegisterWarbandBagContainer(self, bags)
+        DJBagsRegisterBaseBagContainer(self, bags)
+
+        for k, v in pairs(bank) do
+                self[k] = v
+        end
+
+    ADDON.eventManager:Add('BANKFRAME_OPENED', self)
+    ADDON.eventManager:Add('BANKFRAME_CLOSED', self)
+    ADDON.eventManager:Add('PLAYERWARDBANKSLOTS_CHANGED', self)
+end
+
+function bank:BANKFRAME_OPENED()
+        if BankFrame.selectedTab == 3 then
+                self:Show()
+        end
+end
+
+function bank:BANKFRAME_CLOSED()
+        self:Hide()
+end
+
+function bank:PLAYERWARDBANKSLOTS_CHANGED()
+        self:BAG_UPDATE(WARDBANK_CONTAINER)
+end
+
